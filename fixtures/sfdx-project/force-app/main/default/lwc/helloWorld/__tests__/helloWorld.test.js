5374cd6478cda3d33201a13bf7a0b8c967f233f7
junners <jose.junryan@gmail.com>
2026-03-22 12:21:50 -0700

    feat: make lwc spec

---
+++ fixtures/sfdx-project/force-app/main/default/lwc/helloWorld/__tests__/helloWorld.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import HelloWorld from "c/helloWorld";
+
+describe("c-hello-world", () => {
+  it("should exist", () => {
+    expect(HelloWorld).toBeDefined();
+  });
+});
