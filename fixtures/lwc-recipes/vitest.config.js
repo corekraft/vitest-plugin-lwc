2239b1b7323ecf9871b65cba55f604eed782b306
junners <jose.junryan@gmail.com>
2026-03-22 20:00:53 -0700

    feat: migration step 1 add configs

---
+++ fixtures/lwc-recipes/vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import lwc from "@corekraft/vitest-plugin-lwc";
+
+export default defineConfig({
+  plugins: [lwc()],
+  test: {
+    include: ["**/lwc/**/*.test.js"],
+    coverage: {
+      provider: "v8",
+      reporter: ["clover", "cobertura", "lcov", "text", "text-summary"]
+    }
+  }
+});
