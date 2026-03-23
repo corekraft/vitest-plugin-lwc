e5ce3f9695c9a6c4ed556817c17444a3e68357a6
junners <jose.junryan@gmail.com>
2026-03-23 00:46:58 -0700

    chore: semantic release patches

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,23 @@
 const isStableRelease = process.env.SEMANTIC_RELEASE_STABLE === "true";
+const conventionalCommitTypes = [
+  { type: "feat", section: "Features" },
+  { type: "fix", section: "Bug Fixes" },
+  { type: "perf", section: "Performance" },
+  { type: "revert", section: "Reverts" },
+  { type: "docs", section: "Documentation" },
+  { type: "style", section: "Styles" },
+  { type: "refactor", section: "Refactoring" },
+  { type: "test", section: "Tests" },
+  { type: "build", section: "Build System" },
+  { type: "ci", section: "Continuous Integration" },
+  { type: "chore", section: "Chores" },
+];
+const releaseRules = conventionalCommitTypes.map(({ type }) => ({ type, release: "patch" }));
+const noteTypes = conventionalCommitTypes.map(({ type, section }) => ({
+  type,
+  section,
+  hidden: false,
+}));
 
 const githubPluginOptions = isStableRelease
   ? {}
@@ -17,12 +36,16 @@ export default {
       "@semantic-release/commit-analyzer",
       {
         preset: "conventionalcommits",
+        releaseRules,
       },
     ],
     [
       "@semantic-release/release-notes-generator",
       {
         preset: "conventionalcommits",
+        presetConfig: {
+          types: noteTypes,
+        },
       },
     ],
     [
